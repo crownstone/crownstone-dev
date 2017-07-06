989901aa98bba8f89f02878c3c8ea0ccd2b1b13f
Vliedel <bart@dobots.nl>
2017-07-06 16:05:25 +0200

    Removed set/get time and config from main control.

--- app/src/main/java/nl/dobots/crownstone/gui/control/ControlMainFragment.java
+++ app/src/main/java/nl/dobots/crownstone/gui/control/ControlMainFragment.java
@@ -69,10 +69,6 @@ public class ControlMainFragment extends Fragment {
 	private ImageView _lightBulb;
 	private CheckBox _cbPwmEnable;
 
-	private Button _btnSetTime;
-	private Button _btnGetTime;
-	private EditText _txtTime;
-
 	private Button _btnRelayOff;
 	private Button _btnRelayOn;
 	private Button _btnPwmOff;
@@ -82,9 +78,6 @@ public class ControlMainFragment extends Fragment {
 	private ImageButton _btnZoomIn;
 	private ImageButton _btnZoomOut;
 	private ImageButton _btnZoomReset;
-	private EditText _txtConfig;
-//	private Button _btnConfigSet;
-//	private Button _btnConfigGet;
 
 	private RelativeLayout _layStatistics;
 	private RelativeLayout _layControl;
@@ -348,22 +341,6 @@ public class ControlMainFragment extends Fragment {
 			}
 		});
 
-		_btnSetTime = (Button) v.findViewById(R.id.btnSetTime);
-		_btnSetTime.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				setTime();
-			}
-		});
-		_btnGetTime = (Button) v.findViewById(R.id.btnGetTime);
-		_btnGetTime.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				getTime();
-			}
-		});
-		_txtTime = (EditText) v.findViewById(R.id.txtTime);
-
 		_cbPwmEnable = (CheckBox) v.findViewById(R.id.cbPwmEnable);
 		_cbPwmEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			@Override
@@ -424,22 +401,6 @@ public class ControlMainFragment extends Fragment {
 				_graph.resetZoom();
 			}
 		});
-
-//		_txtConfig = (EditText) v.findViewById(R.id.txtConfig);
-//		_btnConfigGet = (Button) v.findViewById(R.id.btnConfigGet);
-//		_btnConfigGet.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				getConfig();
-//			}
-//		});
-//		_btnConfigSet = (Button) v.findViewById(R.id.btnConfigSet);
-//		_btnConfigSet.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				setConfig();
-//			}
-//		});
 
 		enablePwm(_pwmEnabled);
 		_handler.postDelayed(_advStateChecker, 2000);
@@ -865,166 +826,6 @@ public class ControlMainFragment extends Fragment {
 		_btnPwmOn.setEnabled(_pwmEnabled);
 		_sbPwm.setEnabled(_pwmEnabled);
 	}
-
-	private void setTime() {
-		showProgressSpinner();
-		_handler.post(new ControlMainFragment.SequentialRunner("setTime") {
-			@Override
-			public boolean execute() {
-
-				// Or use Calendar.getInstance().getTime() / 1000;
-				long unixTime = System.currentTimeMillis() / 1000;
-				_ble.writeSetTime(_address, unixTime, new IStatusCallback() {
-					@Override
-					public void onSuccess() {
-						Log.i(TAG, "set time success");
-						done();
-						dismissProgressSpinner();
-					}
-
-					@Override
-					public void onError(int error) {
-						Log.i(TAG, "set time failed: " + error);
-						displayError(error);
-						done();
-						dismissProgressSpinner();
-					}
-				});
-				return true;
-			}
-		});
-	}
-
-	private void getTime() {
-		showProgressSpinner();
-		_handler.post(new ControlMainFragment.SequentialRunner("getTime") {
-			@Override
-			public boolean execute() {
-
-				BleExtState bleState = new BleExtState(_ble);
-				bleState.getTime(_address, new IIntegerCallback() {
-					@Override
-					public void onSuccess(int result) {
-						Log.i(TAG, "get time success");
-						long unixTime = BleUtils.toUint32(result);
-						final java.util.Date time = new java.util.Date(unixTime*1000);
-//						Calendar calendar = new Calendar();
-						getActivity().runOnUiThread(new Runnable() {
-							@Override
-							public void run() {
-								_txtTime.setText(time.toString());
-							}
-						});
-
-						done();
-						dismissProgressSpinner();
-					}
-
-					@Override
-					public void onError(int error) {
-						Log.i(TAG, "get time failed: " + error);
-						displayError(error);
-						done();
-						dismissProgressSpinner();
-					}
-				});
-				return true;
-			}
-		});
-	}
-
-	private void getConfig() {
-
-		// Currently sets the relay high duration in ms
-		getConfig(_address, new IIntegerCallback() {
-			@Override
-			public void onSuccess(int result) {
-				final String resStr = Integer.toString(result);
-				getActivity().runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						_txtConfig.setText(resStr);
-					}
-				});
-				showToast("Success");
-			}
-
-			@Override
-			public void onError(int error) {
-				showToast("Get config failed: " + error);
-			}
-		});
-	}
-
-	private void setConfig() {
-		// Currently sets the relay high duration in ms
-		int relayHighDurationMs;
-		String configStr = _txtConfig.getText().toString();
-		try {
-			relayHighDurationMs = Integer.parseInt(configStr);
-		} catch (NumberFormatException e) {
-			showToast("Invalid number");
-			return;
-		}
-		BleLog.getInstance().LOGi(TAG, "setConfig: " + configStr + " = " + relayHighDurationMs);
-
-		setConfig(_address, relayHighDurationMs, new IStatusCallback() {
-			@Override
-			public void onSuccess() {
-				showToast("Success");
-			}
-
-			@Override
-			public void onError(int error) {
-				showToast("Set config failed: " + error);
-			}
-		});
-	}
-
-
-
-	private void getConfig(final IIntegerCallback callback) {
-		if (_ble.isConnected(callback) && _ble.hasConfigurationCharacteristics(callback)) {
-			_bleConfiguration.getRelayHighDuration(_address, callback);
-//			_bleConfiguration.getPwmPeriod(_address, callback);
-		}
-	}
-
-	public void getConfig(String address, final IIntegerCallback callback) {
-//		if (_ble.checkConnection(address)) {
-//			getConfig(callback);
-//		} else {
-			_ble.connectAndExecute(address, new IExecuteCallback() {
-				@Override
-				public void execute(final IExecStatusCallback execCallback) {
-					getConfig(execCallback);
-				}
-			}, new SimpleExecStatusCallback(callback));
-//		}
-	}
-
-	private void setConfig(int value, IStatusCallback callback) {
-		if (_ble.isConnected(callback) && _ble.hasConfigurationCharacteristics(callback)) {
-			_bleConfiguration.setRelayHighDuration(_address, value, callback);
-//			_bleConfiguration.setPwmPeriod(_address, value, callback);
-		}
-	}
-
-	private void setConfig(String address, final int value, final IStatusCallback callback) {
-//		if (_ble.checkConnection(address)) {
-//			setConfig(value, callback);
-//		} else {
-			_ble.connectAndExecute(address, new IExecuteCallback() {
-				@Override
-				public void execute(final IExecStatusCallback execCallback) {
-					setConfig(value, callback);
-				}
-			}, new SimpleExecStatusCallback(callback));
-//		}
-	}
-
-
-
 
 	private void showProgressSpinner() {
 		ProgressSpinner.show(getActivity());
