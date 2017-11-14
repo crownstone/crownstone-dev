e7e5bb49d422845827098fbb4708a026542719fb
Vliedel <bart@dobots.nl>
2017-11-14 17:07:02 +0100

    Changed mesh packets according to protocol 0.12.

--- app/src/main/java/nl/dobots/crownstone/gui/control/ControlMeshFragment.java
+++ app/src/main/java/nl/dobots/crownstone/gui/control/ControlMeshFragment.java
@@ -5,20 +5,21 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
-import nl.dobots.bluenet.ble.base.BleBase;
+import nl.dobots.bluenet.ble.base.structs.ControlMsg;
 import nl.dobots.bluenet.ble.cfg.BluenetConfig;
 import nl.dobots.bluenet.ble.core.callbacks.IStatusCallback;
 import nl.dobots.bluenet.ble.extended.BleExt;
 import nl.dobots.bluenet.ble.mesh.structs.MeshControlMsg;
-import nl.dobots.bluenet.ble.mesh.structs.MeshKeepAlivePacket;
-import nl.dobots.bluenet.ble.mesh.structs.MeshMultiSwitchPacket;
+import nl.dobots.bluenet.ble.mesh.structs.keepalive.MeshKeepAlivePacket;
+import nl.dobots.bluenet.ble.mesh.structs.keepalive.MeshKeepAliveSameTimeoutPacket;
+import nl.dobots.bluenet.ble.mesh.structs.multiswitch.MeshMultiSwitchListPacket;
+import nl.dobots.bluenet.ble.mesh.structs.multiswitch.MeshMultiSwitchPacket;
 import nl.dobots.bluenet.ble.mesh.structs.cmd.MeshControlPacket;
 import nl.dobots.bluenet.utils.BleLog;
 import nl.dobots.bluenet.utils.BleUtils;
@@ -49,42 +50,44 @@ public class ControlMeshFragment extends Fragment {
 //	private Button   _btnMeshMessage;
 
 	// Keep alive
-	private EditText _txtMeshKeepAliveArray;
-	private Button   _btnMeshKeepAliveSend;
-	private Button   _btnMeshKeepAliveClear;
-	private EditText _txtMeshKeepAliveTimeout;
-	private Button   _btnMeshKeepAliveTimeoutSet;
-	private EditText _txtMeshKeepAliveId;
-	private EditText _txtMeshKeepAliveAction;
-	private Button   _btnMeshKeepAliveAddItem;
-	private MeshKeepAlivePacket _meshKeepAlivePacket = new MeshKeepAlivePacket(0);
+	private EditText                       _txtMeshKeepAliveArray;
+	private Button                         _btnMeshKeepAliveSend;
+	private Button                         _btnMeshKeepAliveClear;
+	private EditText                       _txtMeshKeepAliveTimeout;
+	private Button                         _btnMeshKeepAliveTimeoutSet;
+	private EditText                       _txtMeshKeepAliveId;
+	private EditText                       _txtMeshKeepAliveAction;
+	private Button                         _btnMeshKeepAliveAddItem;
+	private MeshKeepAliveSameTimeoutPacket _meshKeepAliveSameTimeoutPacket = new MeshKeepAliveSameTimeoutPacket();
+	private MeshKeepAlivePacket            _meshKeepAlivePacket = new MeshKeepAlivePacket();
 
 	// Multi switch
-	private EditText _txtMeshMultiSwitchArray;
-	private Button   _btnMeshMultiSwitchSend;
-	private Button   _btnMeshMultiSwitchClear;
-	private EditText _txtMeshMultiSwitchId;
-	private EditText _txtMeshMultiSwitchSwitchState;
-	private EditText _txtMeshMultiSwitchTimeout;
-	private EditText _txtMeshMultiSwitchIntent;
-	private Button   _btnMeshMultiSwitchAddItem;
-	private MeshMultiSwitchPacket _meshMultiSwitchPacket = new MeshMultiSwitchPacket();
+	private EditText                  _txtMeshMultiSwitchArray;
+	private Button                    _btnMeshMultiSwitchSend;
+	private Button                    _btnMeshMultiSwitchClear;
+	private EditText                  _txtMeshMultiSwitchId;
+	private EditText                  _txtMeshMultiSwitchSwitchState;
+	private EditText                  _txtMeshMultiSwitchTimeout;
+	private EditText                  _txtMeshMultiSwitchIntent;
+	private Button                    _btnMeshMultiSwitchAddItem;
+	private MeshMultiSwitchListPacket _meshMultiSwitchListPacket = new MeshMultiSwitchListPacket();
+	private MeshMultiSwitchPacket     _meshMultiSwitchPacket = new MeshMultiSwitchPacket();
 
 	// Command control
-	private EditText _txtMeshCommandControlId;
-	private Button   _btnMeshCommandControlAddId;
-	private EditText _txtMeshCommandControl;
-	private Button   _btnMeshCommandControlSet;
-	private EditText _txtMeshCommandControlArray;
-	private Button   _btnMeshCommandControlSend;
-	private Button   _btnMeshCommandControlClear;
+	private EditText          _txtMeshCommandControlId;
+	private Button            _btnMeshCommandControlAddId;
+	private EditText          _txtMeshCommandControl;
+	private Button            _btnMeshCommandControlSet;
+	private EditText          _txtMeshCommandControlArray;
+	private Button            _btnMeshCommandControlSend;
+	private Button            _btnMeshCommandControlClear;
 	private MeshControlPacket _meshCommandControlPacket = new MeshControlPacket();
 
 
 
 	// Other stuff
-	private BleExt _ble;
-	private String _address;
+	private BleExt  _ble;
+	private String  _address;
 	private boolean _closing;
 	private Handler _handler;
 
@@ -98,6 +101,9 @@ public class ControlMeshFragment extends Fragment {
 
 		_ble = ControlActivity.getInstance().getBle();
 		_address = ControlActivity.getInstance().getAddress();
+
+		_meshKeepAlivePacket.setPayload(_meshKeepAliveSameTimeoutPacket);
+		_meshMultiSwitchPacket.setPayload(_meshMultiSwitchListPacket);
 	}
 
 	@Override
@@ -276,6 +282,35 @@ public class ControlMeshFragment extends Fragment {
 		});
 	}
 
+	void writeControlMessage(String text) {
+		text = text.replace("[", ""); // Remove all [
+		text = text.replace("]", ""); // Remove all ]
+		String[] textArr = text.split("[ ,]+"); // Split by space and/or comma
+		byte[] msgBytes = new byte[textArr.length];
+		for (int i=0; i<textArr.length; i++) {
+			msgBytes[i] = (byte)Integer.parseInt(textArr[i]);
+		}
+		BleLog.getInstance().LOGd(TAG, "txt=" + text);
+		BleLog.getInstance().LOGd(TAG, "msg=" + BleUtils.bytesToString(msgBytes));
+
+		ControlMsg msg = new ControlMsg();
+		if (!msg.fromArray(msgBytes)) {
+			BleLog.getInstance().LOGw(TAG, "Invalid message: " + BleUtils.bytesToString(msgBytes));
+			return;
+		}
+		_ble.writeControl(_address, msg, new IStatusCallback() {
+			@Override
+			public void onSuccess() {
+				BleLog.getInstance().LOGd(TAG, "mesh message success");
+			}
+
+			@Override
+			public void onError(int error) {
+				BleLog.getInstance().LOGe(TAG, "mesh message error: " + error);
+			}
+		});
+	}
+
 
 	//////////////////
 	// Mesh message //
@@ -295,34 +330,35 @@ public class ControlMeshFragment extends Fragment {
 	void writeKeepAlive() {
 		BleLog.getInstance().LOGd(TAG, "writeKeepAlive");
 		String text = _txtMeshKeepAliveArray.getText().toString();
-		writeMeshMessage(text);
+		writeControlMessage(text);
 	}
 
 	void addKeepAliveItem() {
 		BleLog.getInstance().LOGd(TAG, "addKeepAliveItem");
 		int id = Integer.parseInt(_txtMeshKeepAliveId.getText().toString());
 		int action = Integer.parseInt(_txtMeshKeepAliveAction.getText().toString());
-		_meshKeepAlivePacket.addKeepAlive(id, action);
+		_meshKeepAliveSameTimeoutPacket.addItem(id, action);
 		updateKeepAliveText();
 	}
 
 	void setKeepAliveTimeout() {
 		BleLog.getInstance().LOGd(TAG, "setKeepAliveTimeout");
 		int timeout = Integer.parseInt(_txtMeshKeepAliveTimeout.getText().toString());
-		_meshKeepAlivePacket.setTimeout(timeout);
+		_meshKeepAliveSameTimeoutPacket.setTimeout(timeout);
 		updateKeepAliveText();
 	}
 
 	void clearKeepAlive() {
 		BleLog.getInstance().LOGd(TAG, "clearKeepAlive");
 		int timeout = Integer.parseInt(_txtMeshKeepAliveTimeout.getText().toString());
-		_meshKeepAlivePacket = new MeshKeepAlivePacket(0);
+		_meshKeepAliveSameTimeoutPacket = new MeshKeepAliveSameTimeoutPacket();
+		_meshKeepAlivePacket.setPayload(_meshKeepAliveSameTimeoutPacket);
 		updateKeepAliveText();
 	}
 
 	void updateKeepAliveText() {
 		byte[] payload = _meshKeepAlivePacket.toArray();
-		MeshControlMsg msg = new MeshControlMsg(BluenetConfig.MESH_HANDLE_KEEP_ALIVE, payload.length, payload);
+		ControlMsg msg = new ControlMsg(BluenetConfig.CMD_KEEP_ALIVE_MESH, payload.length, payload);
 		String text = BleUtils.bytesToString(msg.toArray());
 		_txtMeshKeepAliveArray.setText(text);
 	}
@@ -335,12 +371,13 @@ public class ControlMeshFragment extends Fragment {
 	void writeMultiSwitch() {
 		BleLog.getInstance().LOGd(TAG, "writeMultiSwitch");
 		String text = _txtMeshMultiSwitchArray.getText().toString();
-		writeMeshMessage(text);
+		writeControlMessage(text);
 	}
 
 	void clearMultiSwitch() {
 		BleLog.getInstance().LOGd(TAG, "clearMultiSwitch");
-		_meshMultiSwitchPacket = new MeshMultiSwitchPacket();
+		_meshMultiSwitchListPacket = new MeshMultiSwitchListPacket();
+		_meshMultiSwitchPacket.setPayload(_meshMultiSwitchListPacket);
 		updateMultiSwitchText();
 	}
 
@@ -350,14 +387,14 @@ public class ControlMeshFragment extends Fragment {
 		int switchState = Integer.parseInt(_txtMeshMultiSwitchSwitchState.getText().toString());
 		int timeout = Integer.parseInt(_txtMeshMultiSwitchTimeout.getText().toString());
 		int intent = Integer.parseInt(_txtMeshMultiSwitchIntent.getText().toString());
-		_meshMultiSwitchPacket.addMultiSwitch(crownstoneId, switchState, timeout, intent);
+		_meshMultiSwitchListPacket.addItem(crownstoneId, switchState, timeout, intent);
 		updateMultiSwitchText();
 	}
 
 	void updateMultiSwitchText() {
 		BleLog.getInstance().LOGd(TAG, "updateMultiSwitchText");
 		byte[] payload = _meshMultiSwitchPacket.toArray();
-		MeshControlMsg msg = new MeshControlMsg(BluenetConfig.MESH_HANDLE_MULTI_SWITCH, payload.length, payload);
+		ControlMsg msg = new ControlMsg(BluenetConfig.CMD_MULTI_SWITCH, payload.length, payload);
 		String text = BleUtils.bytesToString(msg.toArray());
 		_txtMeshMultiSwitchArray.setText(text);
 	}
